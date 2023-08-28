c24ff77fd98d09bc905a34015dd551073facaedb
Aurah875 <awinolucy17@gmail.com>
2023-08-27 23:23:08 -0700

    add

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,15 @@ char *_strcat(char *dest, char *src)
 {
 	int length_of_string, z;
 
-	lenght_of_string = 0;
-	while (dest[lenght_of_string] != "\0")
+	length_of_string = 0;
+	while (dest[length_of_string] != '\0')
 	{
 		length_of_string++;
 	}
-	for (z = 0 : src[z] != "\0" : z++, length_of_string++)
+	for (z = 0; src[z] != '\0'; z++, length_of_string++)
 	{
 		dest[length_of_string] = src[z];
 	}
-	dest[length_of_string] = "\0";
+	dest[length_of_string] = '\0';
 	return (dest);
 }
